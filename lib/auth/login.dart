e453f19059fd4a0ea33a47f3b6aa9150705b2a88
IKENNA COLLINS <obettaikenna19@gmail.com>
2023-07-08 07:15:36 +0100

    my second commit

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -30,7 +30,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(children: [],),
     );
   }
 }
